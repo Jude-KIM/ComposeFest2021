85533ae03ff3b6efcb74897f1533e84394a8c65b
Jude <jude0709@gmail.com>
2021-11-01 14:40:16 +0900

    test

--- week 1-Jetpack Compose basics/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ week 1-Jetpack Compose basics/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -58,9 +58,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            BasicsCodelabTheme {
-                MyApp()
-            }
+//            BasicsCodelabTheme {
+//                MyApp()
+//            }
+            Text("Hello world")
         }
     }
 }
