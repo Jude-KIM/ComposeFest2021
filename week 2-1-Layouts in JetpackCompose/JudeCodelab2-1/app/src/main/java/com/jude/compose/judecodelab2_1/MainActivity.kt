7134eade07921ea80e7ccbb30fd7600c18b38911
Jude <jude0709@gmail.com>
2021-11-08 13:57:47 +0900

    2-1 layout 만들기

--- week 2-1-Layouts in JetpackCompose/JudeCodelab2-1/app/src/main/java/com/jude/compose/judecodelab2_1/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/JudeCodelab2-1/app/src/main/java/com/jude/compose/judecodelab2_1/MainActivity.kt
@@ -1,14 +1,52 @@
 package com.jude.compose.judecodelab2_1
 
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.LocalContentAlpha
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.ImagePainter.State.Empty.painter
+import coil.compose.rememberImagePainter
 import com.jude.compose.judecodelab2_1.ui.theme.JudeCodelab21Theme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,23 +54,142 @@ class MainActivity : ComponentActivity() {
         setContent {
             JudeCodelab21Theme {
                 // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                LayoutCodelab()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun LayoutCodelab() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "JudeCodelab") },
+                actions = {
+                    IconButton(onClick = { /* todo */ }) {
+                        Icon(Icons.Filled.Favorite, contentDescription = null)
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+        BodyContent(
+            modifier = Modifier
+                .padding(innerPadding)
+                .padding(8.dp)
+                .background(Color.LightGray)
+        )
+    }
 }
 
-@Preview(showBackground = true)
+@ExperimentalCoilApi
 @Composable
-fun DefaultPreview() {
+fun BodyContent(modifier: Modifier) {
+    val listSize = 100
+// We save the scrolling position with this state
+    val scrollState = rememberLazyListState()
+// We save the coroutine scope where our animated scroll will be executed
+    val coroutineScope = rememberCoroutineScope()
+
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Row {
+            Button(
+                modifier = Modifier.weight(0.5f),
+                onClick = {
+                    coroutineScope.launch {
+                        scrollState.animateScrollToItem(0)
+                    }
+                }) {
+                Text(text = "Go to top")
+            }
+
+            Button(
+                modifier = Modifier.weight(0.5f),
+                onClick = {
+                    coroutineScope.launch {
+                        scrollState.animateScrollToItem(listSize - 1)
+                    }
+                }) {
+                Text(text = "Go to bottom")
+            }
+
+        }
+
+        LazyColumn(state = scrollState) {
+            items(100) {
+                ImageListItem(index = it)
+            }
+        }
+    }
+}
+
+@ExperimentalCoilApi
+@Composable
+fun ImageListItem(index: Int) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(4.dp))
+            .background(Color.Yellow)
+            .clickable { }) {
+        Image(
+            painter = rememberImagePainter(data = "https://developer.android.com/images/brand/Android_Robot.png"),
+            contentDescription = "Android Logo",
+            modifier = Modifier.size(48.dp)
+        )
+        Spacer(modifier = Modifier.width(10.dp).background(MaterialTheme.colors.primary))
+        Text("List Item $index", style = MaterialTheme.typography.subtitle1)
+
+    }
+}
+
+@Preview
+@Composable
+fun LayoutsCodelabPreview() {
     JudeCodelab21Theme {
-        Greeting("Android")
+        Surface(color = Color.White) {
+            LayoutCodelab()
+        }
+
+    }
+}
+
+@Composable
+fun ProfileView(modifier: Modifier = Modifier) {
+    Row(
+        modifier
+            .padding(8.dp)
+            .clip(RoundedCornerShape(4.dp))
+            .background(MaterialTheme.colors.surface)
+            .clickable { }
+            .padding(16.dp)) {
+        Surface(
+            modifier = Modifier.size(48.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+        ) { }
+        Column(
+            modifier = Modifier
+                .padding(start = 8.dp)
+                .align(Alignment.CenterVertically)
+        ) {
+            Text("Jude Aires(김동주)", fontWeight = FontWeight.Bold)
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text("3 minutes ago", style = MaterialTheme.typography.body2)
+            }
+
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ProfilePreview() {
+    JudeCodelab21Theme {
+        Surface(color = MaterialTheme.colors.background) {
+            ProfileView()
+        }
     }
 }
